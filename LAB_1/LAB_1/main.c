33005de2acbfde7f2ae8e186bbb0147ac5bf74aa
Alan Gomez <157183000+KleperForest@users.noreply.github.com>
2024-07-14 19:20:48 -0600

    d

--- LAB_1/LAB_1/main.c
+++ LAB_1/LAB_1/main.c
@@ -1,4 +1,8 @@
+#define F_CPU 16000000UL
+
+#include <stdio.h>
 #include <avr/io.h>
+#include <util/delay.h>
 
 int main(void) {
 	// Configuración del puerto D como salida
@@ -10,13 +14,21 @@ int main(void) {
 	// Establecer todos los pines del puerto D en alto (HIGH)
 	PORTD = 0xFF;  // 0xFF en binario es 11111111, lo que establece todos los pines del puerto D en alto
 	
-	// Establecer PC5 en alto (HIGH) y PC4, PC3 en bajo (LOW)
-	PORTC |= (1 << PC5);  // Establece PC5 en alto
-	PORTC &= ~((1 << PC3) | (1 << PC4));  // Establece PC3 y PC4 en bajo
 
 	// Bucle principal vacío
 	while (1) {
 		// Aquí puede ir el código adicional que quieras ejecutar continuamente
+		
+		// Establecer PC5 en alto (HIGH) y PC4, PC3 en bajo (LOW)
+		PORTC |= (1 << PC5);  // Establece PC5 en alto
+		PORTC &= ~((1 << PC3) | (1 << PC4));  // Establece PC3 y PC4 en bajo
+		
+		
+		PORTC |= (1 << PC3);  // Establece PC5 en alto
+		PORTC &= ~((1 << PC5) | (1 << PC4));  // Establece PC3 y PC4 en bajo
+		
+		PORTC |= (1 << PC4);  // Establece PC5 en alto
+		PORTC &= ~((1 << PC3) | (1 << PC5));  // Establece PC3 y PC4 en bajo
 	}
 	
 	return 0;
