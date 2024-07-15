a79de98084ab2077d60810a87fc6e406290c7a23
Alan Gomez <157183000+KleperForest@users.noreply.github.com>
2024-07-15 09:08:14 -0600

    XC

--- LAB_1/LAB_1/main.c
+++ LAB_1/LAB_1/main.c
@@ -4,18 +4,12 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#DEFINE 
-
 int main(void) {
 	// Configuración del puerto D como salida
 	DDRD = 0xFF;  // 0xFF en binario es 11111111, lo que significa que todos los pines del puerto D son configurados como salida
 	
 	// Configuración de los pines PC3, PC4 y PC5 como salida
 	DDRC |= (1 << PC3) | (1 << PC4) | (1 << PC5);  // Configura PC3, PC4 y PC5 como salidas
-	
-	// Establecer todos los pines del puerto D en alto (HIGH)
-	//PORTD = 0xFF;  // 0xFF en binario es 11111111, lo que establece todos los pines del puerto D en alto
-	
 
 	// Bucle principal vacío
 	while (1) {
@@ -24,9 +18,44 @@ int main(void) {
 		//Establecer PC5 en alto (HIGH) y PC4, PC3 en bajo (LOW)
 		PORTC |= (1 << PC5);  // Establece PC5 en alto Transistor de Display
 		PORTC &= ~((1 << PC3) | (1 << PC4));  // Establece PC3 y PC4 en bajo
-		PORTD = 0b01111111;
-		_delay_ms(6);
+		PORTD = 0b01000000;
+		_delay_ms(10000);
 		
+		//Establecer PC5 en alto (HIGH) y PC4, PC3 en bajo (LOW)
+		PORTC |= (1 << PC5);  // Establece PC5 en alto Transistor de Display
+		PORTC &= ~((1 << PC3) | (1 << PC4));  // Establece PC3 y PC4 en bajo
+		PORTD = 0b00100000;
+		_delay_ms(10000);
+		
+		//Establecer PC5 en alto (HIGH) y PC4, PC3 en bajo (LOW)
+		PORTC |= (1 << PC5);  // Establece PC5 en alto Transistor de Display
+		PORTC &= ~((1 << PC3) | (1 << PC4));  // Establece PC3 y PC4 en bajo
+		PORTD = 0b00010000;
+		_delay_ms(10000);
+		
+		//Establecer PC5 en alto (HIGH) y PC4, PC3 en bajo (LOW)
+		PORTC |= (1 << PC5);  // Establece PC5 en alto Transistor de Display
+		PORTC &= ~((1 << PC3) | (1 << PC4));  // Establece PC3 y PC4 en bajo
+		PORTD = 0b00001000;
+		_delay_ms(10000);
+		
+		//Establecer PC5 en alto (HIGH) y PC4, PC3 en bajo (LOW)
+		PORTC |= (1 << PC5);  // Establece PC5 en alto Transistor de Display
+		PORTC &= ~((1 << PC3) | (1 << PC4));  // Establece PC3 y PC4 en bajo
+		PORTD = 0b00000100;
+		_delay_ms(10000);
+		
+		//Establecer PC5 en alto (HIGH) y PC4, PC3 en bajo (LOW)
+		PORTC |= (1 << PC5);  // Establece PC5 en alto Transistor de Display
+		PORTC &= ~((1 << PC3) | (1 << PC4));  // Establece PC3 y PC4 en bajo
+		PORTD = 0b00000010;
+		_delay_ms(10000);
+		
+		//Establecer PC5 en alto (HIGH) y PC4, PC3 en bajo (LOW)
+		PORTC |= (1 << PC5);  // Establece PC5 en alto Transistor de Display
+		PORTC &= ~((1 << PC3) | (1 << PC4));  // Establece PC3 y PC4 en bajo
+		PORTD = 0b00000001;
+		_delay_ms(10000);
 		
 	}
 	
