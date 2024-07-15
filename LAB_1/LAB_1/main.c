6371be14e26814eb9841600b2ee355c5122733f4
Alan Gomez <157183000+KleperForest@users.noreply.github.com>
2024-07-15 10:05:21 -0600

    d

--- LAB_1/LAB_1/main.c
+++ LAB_1/LAB_1/main.c
@@ -24,12 +24,45 @@ ISR(TIMER1_COMPA_vect) {
 	}
 }
 
+ISR(PCINT0_vect) {
+	// Comprobar si el botón en PB0 está presionado (PB0 es bajo)
+	if (!(PINB & (1 << PB0))) {
+		PORTD = 0xFF;  
+	}
+	// Comprobar si el botón en PB1 está presionado (PB1 es bajo)
+	if (!(PINB & (1 << PB1))) {
+		PORTD = 0x0C; 
+	}
+		
+	// Comprobar si el botón en PB2 está presionado (PB1 es bajo)
+	if (!(PINB & (1 << PB2))) {
+		// Activar Timer.
+		//Establecer BJT_Display en alto (HIGH) y BJT_BLUE, BJT_RED en bajo (LOW)
+		PORTC |= (1 << PC5);  // Establece PC5 en alto 
+		PORTC &= ~((1 << PC3) | (1 << PC4));  // Establece PC3 y PC4 en bajo
+		PORTD = fila[counter];// Mostrar valores en Display de Timer. 
+	}
+	
+}
+
+void pcint_init() {
+	PCICR |= (1 << PCIE0);  // Habilitar interrupciones de cambio de pin para PCINT[7:0]
+	PCMSK0 |= (1 << PCINT0) | (1 << PCINT1) | (1 << PCINT2);  // Habilitar interrupción para PCINT0 (PB0), PCINT1 (PB1) Y PCINT2 (PB2)
+}
+
 int main(void) {
 	// Configuración del puerto D como salida
 	DDRD = 0xFF;  // 0xFF en binario es 11111111, lo que significa que todos los pines del puerto D son configurados como salida
 	
 	// Configuración de los pines PC3, PC4 y PC5 como salida
 	DDRC |= (1 << PC3) | (1 << PC4) | (1 << PC5);  // Configura PC3, PC4 y PC5 como salidas
+	
+	// Configuración de PB0, PB1, PB2 como entradas con resistencias pull-up
+	DDRB &= ~((1 << PB0) | (1 << PB1) | (1 << PB2));  // Configura PB0, PB1, PB2 como entradas
+	PORTB |= (1 << PB0) | (1 << PB1) | (1 << PB2);  // Habilita resistencias pull-up en PB0, PB1, PB2
+	
+	
+	pcint_init();
 	
 	// Inicializar el timer
 	timer1_init();
@@ -40,10 +73,7 @@ int main(void) {
 	// Bucle principal vacío
 	while (1) {
 		// Aquí puede ir el código adicional que quieras ejecutar continuamente
-		//Establecer PC5 en alto (HIGH) y PC4, PC3 en bajo (LOW)
-		PORTC |= (1 << PC5);  // Establece PC5 en alto Transistor de Display
-		PORTC &= ~((1 << PC3) | (1 << PC4));  // Establece PC3 y PC4 en bajo
-		PORTD = fila[counter];// Mostrar valores en Display de Timer. 
+		
 		
 	}
 	
