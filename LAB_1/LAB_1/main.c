e9931c83eb5db895ba3243c0bc99798ee4a770ab
Alan Gomez <157183000+KleperForest@users.noreply.github.com>
2024-07-15 12:22:42 -0600

    s

--- LAB_1/LAB_1/main.c
+++ LAB_1/LAB_1/main.c
@@ -7,29 +7,68 @@
 volatile uint8_t counter = 5; // Valor de cuenta regresiva
 
 const uint8_t fila_1[] = {0x3F, 0x0C, 0x5B, 0x5D, 0x6C, 0x75};  // Valores para los pines de PORTD Display
-const uint8_t fila_2[] = {0x00, 0x40, 0x20, 0x10, 0x80};        // Valores para los pines de PORTD Display
+const uint8_t fila_2[] = {0x00, 0x40, 0x20, 0x10, 0x80};        // Valores para los pines de PORTD Jugadores
 
 volatile uint8_t Bandera_1 = 0;// Bandera para Diplay
-volatile uint8_t Bandera_2 = 0;// Bnadera para jugadores
+volatile uint8_t Bandera_2 = 0;// Bandera para jugadores
 volatile uint8_t P2_RED = 0;// Jugador 2
 volatile uint8_t P1_BLUE = 0;// Jugador 1
 
+volatile uint8_t Win_ner = 0;// Jugador 2 for win
+
+void debounce_delay() {
+	_delay_ms(150);
+}
+
+void incrementar_contador_1() {
+	if(Bandera_2 == 1)
+	{P1_BLUE++;// Incremento de jugador uno
+	Win_ner = (P1_BLUE > 4) ? 1 : 0; //Determinar ganador
+	Bandera_2 = (P1_BLUE > 4) ? 0 : 1;// Parar Carrera
+	}
+	
+}
+
+void incrementar_contador_2() {
+	if(Bandera_2 == 1){
+	P2_RED++;// Incremento de jugador dos
+	Win_ner = (P2_RED > 4) ? 2 : 0; //Determinar ganador
+	Bandera_2 = (P2_RED > 4) ? 0 : 1;// Parar Carrera
+	}
+}
+
+void Winner(){
+	if (Win_ner == 1){
+		// Mostrar valores en Display de Timer (cuenta regresiva)
+		PORTC |= (1 << PC5);   // BJT_Display en alto
+		PORTC &= ~((1 << PC3) | (1 << PC4));  // BJT_BLUE y BJT_RED en bajo
+		
+		PORTD = fila_1[1];  // Mostrar valores en Display de Timer
+	}
+	else if (Win_ner == 2){
+		// Mostrar valores en Display de Timer (cuenta regresiva)
+		PORTC |= (1 << PC5);   // BJT_Display en alto
+		PORTC &= ~((1 << PC3) | (1 << PC4));  // BJT_BLUE y BJT_RED en bajo
+		
+		PORTD = fila_1[2];  // Mostrar valores en Display de Timer	
+	}
+			
+}
+	
 ISR(PCINT0_vect) {
 	// Comprobar si el botón en PB0 está presionado (PB0 es bajo)
 	if (!(PINB & (1 << PB0))) {
 		if (Bandera_2 == 1) {
-			P2_RED++;// Incremento de jugador dos
-			P2_RED = (P2_RED > 4) ? 0 : P2_RED;// Evitar que sobre pase 5
-			_delay_ms(150); // Antirebote
+			incrementar_contador_2(); // incremento para jugador 2
+			debounce_delay();// Antirrebote
 		}
 	}
 	
 	// Comprobar si el botón en PB1 está presionado (PB1 es bajo)
 	if (!(PINB & (1 << PB1))) {
 		if (Bandera_2 == 1) {
-			P1_BLUE++;// Incremento de jugador uno
-			P1_BLUE = (P1_BLUE > 4) ? 0 : P1_BLUE;// Evitar que sobre pase 5
-			_delay_ms(150);// Antirebote
+			incrementar_contador_1(); // incremento para jugador 1
+			debounce_delay();// Antirrebote
 		}
 	}
 
@@ -37,7 +76,11 @@ ISR(PCINT0_vect) {
 	if (!(PINB & (1 << PB2))) {
 		// Activar Timer.
 		Bandera_1 = 1;// Activar decremento de display
-		_delay_ms(150);// Antirebote
+		Bandera_2 = 0;// Desactivar incremento de jugadores
+		counter = 6; // Reiniciar contador
+		P1_BLUE = 0; // Reiniciar valor de jugador 1
+		P2_RED = 0; // Reiniciar valor de jugador 2
+		debounce_delay();// Antirrebote
 		
 	}
 }
@@ -68,6 +111,10 @@ int main(void) {
 	while (1) {
 		// Control de Timer
 		while (Bandera_1) {
+			//Controlar cremento de jugadores
+			P1_BLUE = 0;
+			P2_RED = 0;
+			
 			// Mostrar valores en Display de Timer (cuenta regresiva)
 			PORTC |= (1 << PC5);   // BJT_Display en alto
 			PORTC &= ~((1 << PC3) | (1 << PC4));  // BJT_BLUE y BJT_RED en bajo
@@ -96,8 +143,12 @@ int main(void) {
 			PORTC &= ~((1 << PC3) | (1 << PC5));  // BJT_Display y BJT_BLUE en bajo
 			PORTD = fila_2[P2_RED];  // Mostrar valores en Display
 			_delay_ms(6);
+			
+			Winner();
 
 		}
+		
+		
 	}
 
 	return 0;
